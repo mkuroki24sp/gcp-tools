30efa6276cc4c44d3443324d4fa3ee5394d573bd
mkuroki24sp <mkuroki24sp@gmail.com>
2020-02-18 17:26:32 +0900

    update some codes

--- bin/add-multi-iam-policy.py
+++ bin/add-multi-iam-policy.py
@@ -1,17 +1,29 @@
 import sys
+import ConfigParser
+import json
 import subprocess
 
-if len(sys.argv) == 2:
-    print('if')
-else:
-    print('else')
+# for check file exists
+import os
+import errno
+
+config = ConfigParser.ConfigParser()
+if not os.path.exists('../config.ini'):
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errono.ENOENT), '../config.ini')
+config.read('../config.ini')
+
+projects = json.loads(config.get("iam-roles-config","projects"))
+roles = json.loads(config.get("iam-roles-config","roles"))
 
 args = sys.argv
-commands = ['nslookup', args[1]]
+for project in projects:
+    for role in roles:
+        commands = 'gcloud projects add-iam-policy-binding ' + project + ' --member user:' + args[1] + ' --role ' + role
+        print(commands)
 
-try:
-    res = subprocess.check_call(commands)
-except:
-    print "Error."
+#try:
+#    res = subprocess.check_call(commands)
+#except:
+#    print "Error."
 
-print res
+#print res
