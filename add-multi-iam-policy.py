ab6590a7ab2a37e5a784d6c1d37b38c374cd83a6
mkuroki24sp <mkuroki24sp@gmail.com>
2020-05-28 18:43:10 +0900

    Bug Fix

--- add-multi-iam-policy.py
+++ add-multi-iam-policy.py
@@ -20,7 +20,7 @@ roles = json.loads(config.get("iam-roles-config","roles"))
 args = sys.argv
 for project in projects:
     for role in roles:
-        commands = 'gcloud projects add-iam-policy-binding ' + project + ' --member user:' + args[1] + ' --role ' + role
+        commands = 'gcloud projects add-iam-policy-binding ' + project + ' --member=' + args[1] + ' --role=' + role
         print(commands)
         try:
             res = subprocess.check_call(commands)
